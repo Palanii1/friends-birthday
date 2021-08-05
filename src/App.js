bed80e002b06e16beea47980520ba0ea5ed40f07
Mr Freeman <palani@Mrs-MacBook-Pro.local>
2021-08-05 04:41:03 -0700

    Birthday Reminder
    Reminder for friends birthdays

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import { data } from './data';
+
+
+
 
 function App() {
+
+    const [people, setPeople] = useState(data);
+
+    const[text, setText] = useState("5 birthdays today");
+
+    const handleClick = () => {
+      if (text === "5 birthdays today"){
+        setPeople([])
+        setText ("0 birthdays today");
+      } 
+    }
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <div className="container">
+        <h2>{ text }</h2> 
+        {people.map((person) => {
+                const { id, image, name, age } = person
+                return (
+
+                    <div key={id} className="item">
+                        <div className="image">
+                            <img src={image} alt="" />
+                        </div>
+                        <div className="words">
+                            <div className="name"><h4>{name}</h4></div>
+                            <div className="age"><h5>{age}</h5></div>   
+                        </div>
+                    </div>
+                );
+            })}
+            <button className="btn" onClick = { handleClick }>Clear All</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const data = [
+    { id: 1, image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.ztXfCwFuXWi1C3YbJ7GI-wHaEK%26pid%3DApi&f=1', name: 'Bertie Yates', age: '29 years' },
+    { id: 2, image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Lq4zMJo-stk83WrV5ZjmBAHaEa%26pid%3DApi&f=1', name: 'Hester Hogan', age: '32 years' },
+    { id: 3, image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.H-ReaJOMFt0GaggjzL0MtAHaE7%26pid%3DApi&f=1', name: 'Larry Little', age: '34 years' },
+    { id: 4, image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.G6wo5h2_bZf8y_9B6MeqgAHaDt%26pid%3DApi&f=1', name: 'Sean Walsh', age: '36 years' },
+    { id: 5, image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.mbVvYobbxMyvUzg2_tlGFQHaEK%26pid%3DApi&f=1', name: 'Lola Gardner', age: '38 years' },
+];
